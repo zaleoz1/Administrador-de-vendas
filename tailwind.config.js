ec752a4a7ff73179d54903b6eed234938e01bade
Isaleo Guimaraes <isaleoguimaraes284@gmail.com>
2025-05-27 13:48:59 -0300

    Ajustes no header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cor-primaria': '#15637b',
+        'cor-secundaria': '#10546b',
+        'cor-terciaria': '#0b445b',
+        'cor-quaternaria': '#05354a',
+        'cor-escura': '#00253a',
+      },
+      blue: {
+        500: '#15637b',
+        600: '#10546b',
+        700: '#0b445b',
+      }
+    },
   },
   plugins: [],
 }
